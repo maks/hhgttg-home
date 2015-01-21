68ce2f4ae507749cb31cb5d778bb388ab3ba2b74
Maksim Lin <maks@manichord.com>
2015-01-21 20:55:41 +1100

    fix name in makefile

--- Android.mk
+++ Android.mk
@@ -5,7 +5,7 @@ LOCAL_MODULE_TAGS := optional
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
-LOCAL_PACKAGE_NAME := hhgttg
+LOCAL_PACKAGE_NAME := hhgttg-home
 LOCAL_CERTIFICATE := platform
 
 #LOCAL_PROGUARD_FLAG_FILES := proguard.flags
